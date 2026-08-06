97e29e90f1bab981add6f0feb81970d028bd48ea
Divyanshu Sargara <divyanshbhati1227@gmail.com>
2026-08-06 16:14:00 +0530

    q78.py

--- py/q78.py
+++ py/q78.py
@@ -1,33 +1,73 @@
 # DSA in Python - Generate Parentheses | Recursion & Backtracking | Leetcode 22 - Part 74
 
-def bracket(s):
-    limit = s*2
+
+
+
+# my solution-----------
+# def bracket(s):
+#     limit = s*2
+#     result = []
+#     subset = ["0"]*limit
+
+
+#     def recursion(indx , left, right):
+#         if s == 1:
+#             return result.append("()")
+
+#         if indx >= limit:
+#             return result.append("".join(subset))
+
+
+#         if left > 0:
+#             subset[indx] = "("
+#             recursion(indx+1 , left-1, right)
+            
+
+#         if right > left:
+#             subset[indx] = ")"
+#             recursion(indx+1 , left, right-1)
+            
+            
+#     recursion(0 ,s,s)
+#     return result
+
+# print(bracket(3))
+
+
+# optimal solution--------
+
+def bracket(n):
     result = []
+    limit = n*2
     subset = ["0"]*limit
 
+    def recursion(idx , total):
 
-    def recursion(indx , left, right):
-        if s == 1:
-            return result.append("()")
+        if idx == limit:
+            if total == 0:
+                return result.append("".join(subset))
+            return
 
-        if indx >= limit:
-            return result.append("".join(subset))
+        if total >n:
+            return
+
+        if total < 0:
+            return
+        
+
+        subset[idx] = "("
+        sum = total + 1
+        recursion(idx+1 , sum)
+        subset[idx] = ")"        
+        sum = total - 1
+        recursion(idx+1 , sum)
 
 
-        if left > 0:
-            subset[indx] = "("
-            recursion(indx+1 , left-1, right)
-            
-
-        if right > left:
-            subset[indx] = ")"
-            recursion(indx+1 , left, right-1)
-            
-            
-    recursion(0 ,s,s)
+    recursion(0 , 0)
     return result
 
 print(bracket(3))
+
 
 
 
